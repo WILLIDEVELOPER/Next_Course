37ded8974322cb8752600920fd6856c4801bfd6f
WILLIDEVELOPER <wilsai1023@gmail.com>
2024-10-18 17:18:39 -0500

    some changes

---
+++ my-dashboard/src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,17 @@
+import { PokemonGrid } from "@/pokemons";
+
+export const metadata ={
+    title: "Favorites",
+    description: "favorites page",
+}
+
+export default async function PokemonsPage() {
+  return (
+    <div className="flex flex-col">
+      <span className="text-5xl my-2">
+        Favoritos <small>Global state</small>
+      </span>
+      <PokemonGrid pokemons={[]} />
+    </div>
+  );
+}
--- my-dashboard/src/app/dashboard/main/page.tsx
+++ my-dashboard/src/app/dashboard/main/page.tsx
@@ -1,7 +1,13 @@
+import { SimpleWidget } from "@/app/components";
+
 export default function MainPage() {
-    return (
-        <div>
-            <h1>Main</h1>
-        </div>
-    );
+  return (
+    <div className="text-black px-2">
+      <h1 className="mt-2 text-3xl">Dashboard</h1>
+      <span className="text-xl"> Informacion General</span>
+      <div className="flex flex-wrap p-2 justify-center">
+        <SimpleWidget />
+      </div>
+    </div>
+  );
 }
