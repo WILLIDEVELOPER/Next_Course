89b9546ca397d06e55cfafdaaed9827e7fb7ad45
WILLIDEVELOPER <wilsai1023@gmail.com>
2024-10-27 18:33:02 -0500

    some changes

---
+++ admin-todos/src/app/api/todos/route.ts
@@ -0,0 +1,37 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const take = Number(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+
+  if (isNaN(skip)) {
+    return NextResponse.json(
+      { message: "skip must be a number" },
+      { status: 400 }
+    );
+  }
+
+  if (isNaN(take)) {
+    return NextResponse.json(
+      { message: "take must be a number" },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take: take,
+    skip: skip,
+  });
+
+  return NextResponse.json(todos);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const todo = await prisma.todo.create({
+    data: body,
+  });
+  return NextResponse.json(todo);
+}
