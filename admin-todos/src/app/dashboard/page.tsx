fc9d54f340fb15a2f17095af823658f7b6aa1785
WILLIDEVELOPER <wilsai1023@gmail.com>
2024-10-29 17:52:21 -0500

    doing server actions to learn how to do the same thing as api funcionality but fastly

--- admin-todos/src/app/dashboard/page.tsx
+++ admin-todos/src/app/dashboard/page.tsx
@@ -1,5 +1,9 @@
+import { WidgetItem } from "@/components";
+
 export default function DashboardPage() {
   return (
-    <div>Page Dashboard</div>
-  )
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <WidgetItem />
+    </div>
+  );
 }
