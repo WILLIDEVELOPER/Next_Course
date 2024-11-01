53f0e27d75da38bf2b185a54a28241e9e396e464
WILLIDEVELOPER <wilsai1023@gmail.com>
2024-11-01 17:18:19 -0500

    seccion 14 terminada, empezando a aprender zustand

---
+++ teslo-shop/src/app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AuthHome() {
+  return (
+    <div className="">
+      <h1>Login page</h1>
+    </div>
+  );
+}
---
+++ teslo-shop/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { inter } from "@/config/fonts";
+
+export const metadata: Metadata = {
+  title: "Teslo Shop",
+  description: "Una app de comercio electrónico",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
