831cb070de9d8d1f79fed4ec1e007feed9048ad2
WILLIDEVELOPER <wilsai1023@gmail.com>
2024-11-14 14:42:37 -0500

    seccion 20 ended

--- teslo-shop/src/actions/index.ts
+++ teslo-shop/src/actions/index.ts
@@ -2,3 +2,4 @@ export * from './product/product-pagination';
 export * from './product/product-actions';
 export * from "./auth/login";
 export * from "./auth/logout";
+export * from "./auth/register";
--- teslo-shop/src/app/auth/new-account/ui/RegisterForm.tsx
+++ teslo-shop/src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import clsx from "clsx";
 import Link from "next/link";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import { login, registerUser } from "@/actions";
+import { useState } from "react";
 
 type FormInputs = {
   name: string;
@@ -10,30 +14,66 @@ type FormInputs = {
 };
 
 export const RegisterForm = () => {
-  const { register, handleSubmit } = useForm<FormInputs>();
+  const [errorMessage, setErrorMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    setErrorMessage("");
+    const { name, email, password } = data;
+
+    // Server action
+    const resp = await registerUser(name, email, password);
+
+    if (!resp.ok) {
+      setErrorMessage(resp.message);
+      return;
+    }
+
+    await login(email.toLowerCase(), password);
+    window.location.replace("/");
+  };
 
   return (
-    <div className="flex flex-col">
-      <label htmlFor="email">Nombre Completo</label>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      {/* {
+        errors.name?.type === 'required' && (
+          <span className="text-red-500">* El nombre es obligatorio</span>
+        )
+      } */}
+
+      <label htmlFor="email">Nombre completo</label>
       <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.name,
+        })}
         type="text"
+        autoFocus
         {...register("name", { required: true })}
       />
 
       <label htmlFor="email">Correo electrónico</label>
       <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.email,
+        })}
         type="email"
         {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
       />
 
       <label htmlFor="email">Contraseña</label>
       <input
-        className="px-5 py-2 border bg-gray-200 rounded mb-5"
-        type="email"
-        {...register("password", { required: true })}
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.password,
+        })}
+        type="password"
+        {...register("password", { required: true, minLength: 6 })}
       />
+
+      <span className="text-red-500">{errorMessage} </span>
 
       <button className="btn-primary">Crear cuenta</button>
 
@@ -47,6 +87,6 @@ export const RegisterForm = () => {
       <Link href="/auth/login" className="btn-secondary text-center">
         Ingresar
       </Link>
-    </div>
+    </form>
   );
 };
